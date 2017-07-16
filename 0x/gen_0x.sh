999856e65f354786ce7f6feead96d86e00d8428d
Your Name <you@example.com>
2017-07-15 20:22:19 -0700

    add one express

--- 0x/gen_0x.sh
+++ 0x/gen_0x.sh
@@ -2,6 +2,15 @@
 SRCFILE=$1
 DESTFILE=result.${SRCFILE}
 
+REGEXP1="/^[[:space:]]*$/d"  #去除空行
+REGEXP2=':label;N;s/\n//;b label'  ##去除换行符
+REGEXP3="s/[a-z]/\u&/g"         #字符转换为大写
+REGEXP4="s/\(\w\w\)/0x\1,/g"         #在两个连续字符添加0x,
+
+
+
+
+
 if [ 0 -eq $# ];then
 echo please input one file
 exit
@@ -9,8 +18,16 @@ fi
 
 echo "parsing file " $SRCFILE
 echo "" > $DESTFILE
-cat $SRCFILE | sed 's/\(\w\w\)/0x\1, /g' | sed  /^[[:space:]]*$/d >> $DESTFILE
+cat $SRCFILE |  sed  ${REGEXP1} | sed  ${REGEXP3} | sed  ${REGEXP4} >> $DESTFILE
 echo "" >> $DESTFILE
+
+
+# cat org | sed 's/\(\w\w\)/0x\1,/g' | sed  '/^[[:space:]]*$/d' | sed  's/[[:space:]]*//g'  | sed ':label;N;s/\n//;t label'  
+
+##cat org | sed  ${REGEXP2}
+
+
+# cat org | sed  ${REGEXP1} | sed  ${REGEXP3} | sed  ${REGEXP4}
 
 
 
